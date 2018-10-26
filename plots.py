7c2e6102eb1cbc8c6825f87bb58fca710a269d80
DeLucaJ <delucaj96@gmail.com>
2018-10-26 09:48:16 -0700

    intial code commit

---
+++ plots.py
@@ -0,0 +1,46 @@
+'''
+Title:		Five Room Dungeon Adventure Generator
+Author:		Joseph DeLuca
+Assignment:	CSC 570 | Lab 2 - Episodics
+Professor:	Dr. Foaad Khosmood
+Date:		October 2018
+'''
+# The terminal dictionary
+terminals = {
+	
+}
+
+# Dungeon Entrance with a guardian
+def entrance():
+
+# Puzzle or Roleplaying Challenge
+def challenge():
+
+# Trick or Setback
+def trick():
+
+# The Big Climax / Boss Fight
+def climax():
+
+# Reward or Resolution
+def resolution():
+
+# The Beginning of the Grammar 
+def episode():
+
+	return "{0}{1}{2}{3}{4}".format(
+		entrance(),
+		challenge(), 
+		trick(),
+		climax(),
+		resolution()
+	)
+
+# Main Funtion
+def main():
+
+	plot = episode()
+	print(plot)
+
+if __name__ == '__main__':
+	main()
