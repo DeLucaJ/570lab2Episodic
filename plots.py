08e0d8bb83348089237f0b6f932e07a300db9fd5
DeLucaJ <delucaj96@gmail.com>
2018-10-27 12:56:10 -0700

    added lots of data to plots

--- plots.py
+++ plots.py
@@ -5,20 +5,99 @@ Assignment:	CSC 570 | Lab 2 - Episodic
 Professor:	Dr. Foaad Khosmood
 Date:		October 2018
 '''
-# The terminals
-boss = ["bossA", "bossB", "bossC"],
-condition = ["conditionA", "conditionB", "conditionC"],
-enemy = ["enemyA", "enemyB", "enemyC"],
-environment = ["environmentA", "environmentB", "environmentC"],
-location = ["locationA", "locationB", "locationC"]
-npc = ["npcA", "npcB", "npcC"],
-physicalBarrier = ["physicalBarrierA", "physicalBarrierB", "physicalBarrierC"],
-puzzle = ["puzzleA", "puzzleB", "puzzleC"],
-setback = ["setbackA", "setbackB", "setbackC"],
-trap = ["trapA", "trapB", "trapC"]
 
-# Dungeon Entrance with a guardian
-def entrance():
+import random
+
+# If a class has been used, do not use again
+# A list of dictionaries
+classes = [
+	{"name": "Artificer", "method": "mechanical contraptions"},
+	{"name": "Barbarian", "method": "incredible strength"}, 
+	{"name": "Bard", "method": "irresistible charm"}, 
+	{"name": "Cleric", "method": "divine power"}, 
+	{"name": "Druid", "method": "the power of nature"}, 
+	{"name": "Fighter", "method": "blunt force"},
+	{"name": "Monk", "method": "skillful martial arts"}, 
+	{"name": "Mystic", "method": "psychic abilities"},
+	{"name": "Paladin", "method": "a divine smite"}, 
+	{"name": "Ranger", "method": "unmatched survival skills"}, 
+	{"name": "Rogue", "method": "amazing agility"}, 
+	{"name": "Sorcerer", "method": "innate magic"}, 
+	{"name": "Warlock", "method": "dark secrets"}, 
+	{"name": "Wizard", "method": "unfathomable intellect"}
+]
+
+# A list of dictionaries?
+villain = [
+	"Dragon",
+	"Lich",
+	"Mind Flayer",
+	"Beholder",
+	"Emperyan",
+	"Tarrasque",
+	"Necromancer",
+	"Archmage",
+	"Vampire",
+	"Kraken",
+	"Archdevil",
+	"Archdemon",
+	"Corrupt Angel",
+	"Leviathan",
+	"1,000,000 Goblins"
+]
+
+treasure = [
+	"the shoe of a thousand truths",
+	"the holy sword, Excalibur",
+	"the unholy sword, Rubilacxe",
+	"the abnormally normal sword, Sword"
+	"a tome of lost magical secrets",
+	"a sphere of annihilation",
+	"a talisman of pure good",
+	"a talisman of ultimate evil",
+	"the ultimate shield, Aegis",
+	"the deck of many things",
+	"the book of economancy, The Economicon",
+	"the book of death, The Necronomicon"
+]
+
+escape_route = [
+	"through a secret passage",
+	"by teleporting to another plane"
+	"by transferring their soul to another body",
+	"turning into mist and flying away",
+	"using a henchmen as a meat-shield"
+]
+
+# should probably have attributes
+locations = [
+	"a hidden tower",
+	"a sprawling underground mega-dungeon",
+	"a mysterious forest",
+	"an ancient lost city",
+	"a floating fortress",
+	"an underwater temple",
+	"a castle at the peak of the tallest mountain",
+]
+
+setbacks = [
+	"The {Class} had their {Gear} stolen, and the party got sidetracked retrieving it",
+	"The path to their goal was destroyed so the had to find another way around",
+	"The {Class} was killed in action, and the party had to continue without them",
+	"The {Class} betrayed the party, killing the {Class}, but eventually falling",
+	"The party found their goal, but it was actually an illusion to hide the real goal",
+	"The party got swarmed by {Minions} and needed to escape"
+]
+
+# Grabs and removes a random class from the list.
+def grab_attribute(l):
+
+	item = random.sample(l, 1)
+	l.remove(item[0])
+	return item[0]
+
+# Dungeon location
+def location():
 
 # Puzzle or Role-playing Challenge
 def challenge():
@@ -36,7 +115,7 @@ def resolution():
 def episode():
 
 	return "{0} {1} {2} {3} {4}".format(
-		entrance(),
+		location(),
 		challenge(), 
 		trick(),
 		climax(),
@@ -46,8 +125,8 @@ def episode():
 # Main Function
 def main():
 
-	plot = episode()
-	print(plot)
+	#plot = episode()
+	#print(plot)
 
 if __name__ == '__main__':
 	main()
